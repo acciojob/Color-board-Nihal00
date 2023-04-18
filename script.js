a7143bb9e0fb11e997be82295bdf8590e49e2691
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-04-18 22:49:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+
+for(let i = 0; i < 800; i++) {
+	const box = document.createElement('div');
+	box.className = 'square';
+	container.appendChild(box);
+}
